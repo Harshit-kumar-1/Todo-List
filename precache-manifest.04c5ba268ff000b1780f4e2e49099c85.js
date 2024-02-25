0388fd7cc03822121078849847ceae458b913c80
Harshit-kumar-1 <harshitkumar1026@gmail.com>
2024-02-25 21:25:45 +0530

    Commit first build

---
+++ precache-manifest.04c5ba268ff000b1780f4e2e49099c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc1f60febbd8ab4833fb7f9e7e17d4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "808888250d8667dc7267",
+    "url": "/static/css/main.94e1fc70.chunk.css"
+  },
+  {
+    "revision": "ea47b036ce4b282581ba",
+    "url": "/static/js/2.d7a94695.chunk.js"
+  },
+  {
+    "revision": "536d1ce6027a3623f9eb",
+    "url": "/static/js/3.29f49ed9.chunk.js"
+  },
+  {
+    "revision": "808888250d8667dc7267",
+    "url": "/static/js/main.469114d9.chunk.js"
+  },
+  {
+    "revision": "64be39cf1a5b660c9246",
+    "url": "/static/js/runtime~main.37590d49.js"
+  }
+]);
